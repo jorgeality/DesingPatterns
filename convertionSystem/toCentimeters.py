90afcd84274cc9eaa6e2cd61af16567c253a4946
jorgeality <jorgeality29@gmail.com>
2018-08-29 14:49:55 -0500

    implementing adapter design pattern

--- convertionSystem/toCentimeters.py
+++ convertionSystem/toCentimeters.py
@@ -5,6 +5,10 @@ from pypattyrn.creational.singleton import Singleton
 
 
 #class implemented to convert meters to centimeters
+"""
+    this class is implemented with singleton pattern
+    to create always the same instance of this
+"""
 class toCentimeters(object, metaclass=Singleton):
 
     meters = 0
@@ -13,6 +17,9 @@ class toCentimeters(object, metaclass=Singleton):
         self.meters = value
 
     # method responsible for converting meters to centimeters
+    """
+        the value returned by this method is used to implement adapter pattern in the main class
+    """
     def convert(self):
         centimeters = self.meters * 100
         return str(self.meters) +' meters is/are ' +str(centimeters) +' centimeters'
--- convertionSystem/toMeters.py
+++ convertionSystem/toMeters.py
@@ -2,6 +2,10 @@
 from pypattyrn.creational.singleton import Singleton
 
 #class implemented to convert centimeters to meters
+"""
+    this class is implemented with singleton pattern
+    to create always the same instance of this
+"""
 class toMeters(object, metaclass=Singleton):
 
     centimeters = 0
@@ -11,6 +15,9 @@ class toMeters(object, metaclass=Singleton):
         self.centimeters = value
 
     # method responsible for converting centimeters to meters
+    """
+        the value returned by this method is used to implement adapter pattern in the main class
+    """
     def convert(self):
         meters =  self.centimeters / 100
         return str(self.centimeters)+' centimeters is/are '+str(meters)+' meters'
